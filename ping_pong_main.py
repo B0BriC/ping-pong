b1706b4246a83d35da1ab3ac2b2ab918c12bbc65
B0BriC <103368520+B0BriC@users.noreply.github.com>
2022-04-10 16:53:08 +0300

    create class player

--- ping_pong_main.py
+++ ping_pong_main.py
@@ -11,3 +11,14 @@ class GameSprite(sprite.Sprite):
 
     def reset(self):
         window.blit(self.image, (swlf.rect.x, self.y))
+        
+class Player(GameSprite):
+    def update_1(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_1(self):
+        keys = key.get_pressed()
+        if keys[]
